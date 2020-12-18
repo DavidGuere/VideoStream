8d4e4bd08c0bd16f1c794125ac20c9fe054193ab
GDavid <davidgallardoguere@gmail.com>
2020-12-18 22:21:02 +0100

    error page

---
+++ src/error.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Error() {
+  return (
+    <React.Fragment>
+      <h1>The page does not exist</h1>
+      <img
+        src="https://www.shareicon.net/data/512x512/2016/07/01/789124_interface_512x512.png"
+        alt=""
+      />
+    </React.Fragment>
+  );
+}
+
+export default Error;
