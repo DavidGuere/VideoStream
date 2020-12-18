a8160fe172a29411119d307e9f8d530fb9f8f206
GDavid <davidgallardoguere@gmail.com>
2020-12-18 22:20:38 +0100

    adding search bar and search button

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useForm from "./useForm";
 
 function Nav() {
+  const { HandleChange, value } = useForm();
+
   return (
-    <div className="item">
-      <Link to="/">Home page</Link>
-    </div>
+    <React.Fragment>
+      <div className="header">
+        <div>
+          <Link className="header__link" to="/">
+            Home page
+          </Link>
+        </div>
+        <div>
+          <div>
+            <input
+              className="input"
+              type="text"
+              placeholder="search"
+              name="searchBar"
+              value={value.searchBar}
+              onChange={HandleChange}
+            />
+            <Link className="header__link" to={`/results/${value.searchBar}`}>
+              Search
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
