e6e1451887be530c71548c2c3c4317483f0f70c2
GDavid <davidgallardoguere@gmail.com>
2020-12-18 22:19:22 +0100

    adding error and results page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "./Card";
 import Nav from "./Nav";
+import Results from "./results";
+import Error from "./error";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Placeholder from "./placeholder";
 
@@ -14,6 +16,12 @@ function HomePage() {
             <Card />
           </Route>
           <Route path="/video/:videoURL" children={<Placeholder />}></Route>
+          <Route path="/results/:search">
+            <Results />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
         </Switch>
       </Router>
     </>
